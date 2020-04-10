85da322fdaf42900d752226ddd7ebe496f7e5c5b
Nikita Dyshkant <nikitadyshkant@gmail.com>
2020-04-11 00:23:00 +0300

    second commit

--- js/1.js
+++ js/1.js
@@ -1,12 +1,13 @@
-const cartButton = document.querySelector("#cart-button")
-const modal = document.querySelector(".modal")
-const close = document.querySelector(".close")
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
 
 cartButton.addEventListener('click', function (event) {
-    modal.classList.add("is-open")
-})
+    modal.classList.add("is-open");
+});
 
 close.addEventListener('click', function (event) {
-    modal.classList.remove("is-open")
-})
+    modal.classList.remove("is-open");
+});
+new WOW().init();
